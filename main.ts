8dc9f4c53c0c6a9985b58fffe43dba5509ea2a6f
rachelcnc <115052360+rachelcnc@users.noreply.github.com>
2022-10-12 09:37:18 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
-basic.forever(function () {
-	
+let counter1 = 0
+input.onButtonPressed(Button.A, function () {
+    counter1 = 0
+    while (counter1 <= 4) {
+        led.plot(counter1, 0)
+        basic.pause(1000)
+        basic.clearScreen()
+        counter1 += 1
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("2d03 Rachel Chan")
 })
